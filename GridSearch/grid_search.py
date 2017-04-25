0a3af53b80ffac256519f51469b597636ff37de7
yonedahayato <yhayato1320@gmail.com>
2017-04-25 18:16:15 +0900

    add grid search

---
+++ GridSearch/grid_search.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import inspect
+import sys
+
+def grid_search(function, tuned_parameters, verbose=True):
+
+    key_list = []
+    max_index = []
+    for k in tuned_parameters.keys():
+        key_list.append(k)
+        max_index.append(len(tuned_parameters[k]))
+    print(key_list)
+
+    def execute(index, function, verbose=True):
+        # argument と function を用意して実行する #
+        # return...score（未実装）
+        if verbose: print(index)
+
+        arg_str = ""
+        for i in range(len(key_list)):
+            Index = index[i]
+            arg_str += (key_list[i]+"="+str( tuned_parameters[key_list[i]][Index] ))
+            if i != len(key_list)-1:
+                arg_str += ","
+
+        Score = eval("function("+arg_str+")")
+        if verbose: print(Score)
+        return Score
+
+    def execute_for(index, j, Best_Score, Best_Index, verbose=False):
+        for i in range(max_index[j]): # 0
+            if j==0:
+                Score = execute(index, function, verbose=verbose)
+                if Best_Score < Score:
+                    Best_Score = Score
+                    Best_Index = index
+                index[j] += 1 # 0
+            else:
+                index, Best_Score, Best_Index = execute_for(index, j-1, Best_Score, Best_Index, verbose=verbose)
+
+        index[j] = 0 # 0
+        index[j+1] += 1 # 1
+
+        return index, Best_Score, Best_Index
+
+    Best_Score = 0
+    Best_Index = None
+
+    index = [0]*len(key_list) # start index
+    index, Best_Score, Best_Index = execute_for(index, len(key_list)-1,
+                                                Best_Score, Best_Index, verbose=verbose)
+
+    print("Best_Score: {}".format(Best_Score))
+    Best_Parameters = {}
+    for i, k in enumerate(key_list):
+        Best_Parameters[k] = tuned_parameters[k][Best_Index[i]]
+    print(Best_Parameters)
--- sklearn_MLP.py
+++ sklearn_MLP.py
@@ -2,10 +2,13 @@
 
 from sklearn import datasets
 from sklearn.neural_network import MLPClassifier
+from sklearn.grid_search import GridSearchCV
 from b.bayesian_optimization import BayesianOptimization
 
 import sys
 from collections import OrderedDict
+from sklearn.svm import SVC
+
 
 #data
 digits = datasets.load_digits()
@@ -13,41 +16,58 @@ n_sample = len(digits.images)
 X = digits.images.reshape((n_sample, -1))
 y = digits.target
 
-def MLP(alpha, lr, layer1, layer2, layer3, layer4, layer5):
+train_size = int(n_sample*0.8)
+X_train, X_test = X[:train_size, :], X[train_size:, :]
+y_train, y_test = y[:train_size], y[train_size:]
+
+
+def MLP(alpha, lr, layer1, layer2, layer3):
+    # :::hyper parameters:::
+    #
     # alpha ... L2 penalty
     # lr ... learning_rate
-    mlp = MLPClassifier(hidden_layer_sizes=(int(layer1), int(layer2), int(layer3), int(layer4), int(layer5)),
+    # layer_n
+
+    mlp = MLPClassifier(hidden_layer_sizes=(int(layer1), int(layer2), int(layer3)),
                         max_iter=400, solver="sgd",
                         alpha=alpha, learning_rate_init=lr)
-
-    train_size = int(n_sample*0.8)
-    X_train, X_test = X[:train_size, :], X[train_size:, :]
-    y_train, y_test = y[:train_size], y[train_size:]
 
     mlp.fit(X_train, y_train)
     #print("Test set score: {}".format(mlp.score(X_test, y_test)))
     return mlp.score(X_test, y_test)
 
+
+def grid_search(verbose=True):
+    tuned_parameters = {"alpha": [10**i for i in range(-8, -4+1, 1)],
+                        "lr": [10**i for i in range(-6, -2+1, 1)],
+                        "layer1": [10*i for i in range(1, 10+1, 1)],
+                        "layer2": [10*i for i in range(1, 10+1, 1)],
+                        "layer3": [10*i for i in range(1, 10+1, 1)]}
+
+    from GridSearch.grid_search import grid_search
+    grid_search(MLP, tuned_parameters, verbose=verbose)
+
+
 def main(k_num, acq):
     gp_params = {"alpha": 1e-5}
     BO = BayesianOptimization(MLP,
                               {"alpha": (1e-8, 1e-4), "lr": (1e-6, 1e-2),
-                               "layer1": (10, 100),"layer2": (10, 100),"layer3": (10, 100),"layer4": (10, 100),"layer5": (10, 100)},
+                               "layer1": (10, 100),"layer2": (10, 100),"layer3": (10, 100)},
                               verbose=1, kernel_num = k_num)
 
     BO.explore({"alpha": [1e-8, 1e-8, 1e-4, 1e-4],"lr": [1e-6, 1e-2, 1e-6, 1e-2],
-                "layer1": [10, 50, 100, 50], "layer2": [10, 50, 100, 50],"layer3": [10, 50, 100, 50],"layer4": [10, 50, 100, 50],"layer5": [10, 50, 100, 50] })
+                "layer1": [10, 50, 100, 50], "layer2": [10, 50, 100, 50],"layer3": [10, 50, 100, 50]})
 
     BO.maximize(n_iter=10, acq=acq, **gp_params)
 
     print("-"*53)
     print("Final Results")
     print("kernel: {}".format(str(BO.kernel)))
+    print("acquisition function: {}".format(BO.acquisition))
+
     print("score: {}".format(BO.res["max"]["max_val"]))
     print("best_parameter: ")
     print(BO.res["max"]["max_params"])
-    print("acquisition function")
-    print(BO.acquisition)
 
     final_result = OrderedDict( (("kernel", str(BO.kernel)),
                                  ("acquisition", BO.acquisition),
@@ -57,6 +77,9 @@ def main(k_num, acq):
     return final_result
 
 
-
 if __name__ == "__main__":
-    main(0, "ucb")
+    #main(0, "ucb")  # Bayesian Optimization
+                    # kernel...Matern(nu=0.5)
+                    # acquisition function...ucb
+
+    grid_search(verbose=True) # 比較用の Grid Search
